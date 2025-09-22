a2b5a3556c07dec8ddc59af86644ec8979a77ab3
habibullah <habibullahkhandokar01@gmail.com>
2025-09-22 16:09:05 +0600

    about image update

--- src/app/components/Profilebanner.jsx
+++ src/app/components/Profilebanner.jsx
@@ -5,14 +5,28 @@ import './viralGrass.css'
 export default function Profilebanner() {
   return (
     <>
-      <div className='relative profie-effect h-[15rem] w-[15rem] flex items-center justify-center rounded-xl border-2'>
-      <div className='w-full h-full  rounded-xl'></div>
+    <div>
+      <div className='relative profie-effect h-[20rem] w-[15rem] flex items-center justify-center'>
+
+
+      <div 
+      className='w-full h-full border-2 border-[--clr-primary]
+      bg-white/20 rounded-xl rotate-3
+      
+      '>
+        
+      </div>
+
         <img
-          className='h-full w-full object-cover rounded-xl absolute'
+          className='
+          h-full w-full object-cover rounded-xl absolute duration-500
+          border-2 border-[--clr-primary] -rotate-3 hover:rotate-0
+          '
           src='./profile.png'
           alt='Profile'
         />
       </div>
+    </div>
     </>
   )
 }
