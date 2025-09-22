d7eadce6351a45e5f10cc2a70478a56c54f58ddd
habibullah <habibullahkhandokar01@gmail.com>
2025-09-22 16:22:55 +0600

    about image hover update

--- src/app/components/Profilebanner.jsx
+++ src/app/components/Profilebanner.jsx
@@ -6,21 +6,15 @@ export default function Profilebanner() {
   return (
     <>
     <div>
-      <div className='relative profie-effect h-[20rem] w-[15rem] flex items-center justify-center'>
-
-
-      <div 
-      className='w-full h-full border-2 border-[--clr-primary]
-      bg-white/20 rounded-xl rotate-3
-      
-      '>
-        
-      </div>
-
+      <div className='relative profie-effect h-[20rem] w-[15rem] flex items-center justify-center group'>
+        <div 
+          className='w-full h-full border-2 border-[--clr-primary] bg-white/20 rounded-xl duration-500 rotate-3 group-hover:-rotate-6 pointer-events-none'
+        >
+        </div>
         <img
           className='
-          h-full w-full object-cover rounded-xl absolute duration-500
-          border-2 border-[--clr-primary] -rotate-3 hover:rotate-0
+            h-full w-full object-cover rounded-xl absolute duration-500 z-10
+            border-2 border-[--clr-primary] -rotate-3 group-hover:rotate-0
           '
           src='./profile.png'
           alt='Profile'
