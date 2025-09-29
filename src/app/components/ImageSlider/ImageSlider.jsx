abb310e4238de1e8820828d21cff45e8ce2bd114
habibullah <habibullahkhandokar01@gmail.com>
2025-09-30 00:23:27 +0600

    image slider button added

--- src/app/components/ImageSlider/ImageSlider.jsx
+++ src/app/components/ImageSlider/ImageSlider.jsx
@@ -1,31 +1,43 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ImageCard from "./ImageCard";
 
 export default function ImageSlider({images}) {
-const [img, setImg] = useState('');
-  useEffect(()=>{
-    if(images?.length > 0){
-      setImg(images[0])
-    }
-  }, [])
+const [currentIndex, setCurrentIndex] = useState(0);
+
   
-  function handleImageClick(img){
-   setImg(img)
+  function handleImageClick(index){
+   setCurrentIndex(index)
   }
+
+  function handleForwordClick(){
+  setCurrentIndex((prev) => (prev + 1) % images.length);
+  }
+  
+  function handleBacwordClick(){
+  setCurrentIndex((prev) => prev === 0 ? images.length -1 : prev - 1);
+  }
+
   return (
     <>
     <div className="py-4">
-    <div className="rounded-lg h-72 mb-4">
-      <img className="object-cover object-top rounded-lg w-full h-full" src={img}/>
+    <div className="rounded-lg h-72 mb-4 relative">
+      <img className="object-cover object-top rounded-lg w-full h-full" src={images[currentIndex]}/>
+
+      <div className="absolute w-full top-1/2">
+      <div className="flex justify-between text-black">
+        <button onClick={()=>handleBacwordClick()}>after</button>
+        <button onClick={()=>handleForwordClick()}>before</button>
+      </div>
+      </div>
     </div>
 
         <div
          className="flex gap-2 overflow-x-auto">
       {images?.map((image, idx) => (
         <div
-        onClick={() => handleImageClick(image)}
+        onClick={() => handleImageClick(idx)}
         >
         <ImageCard img={image} idx={idx}/>
         </div>
