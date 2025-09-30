841d1de9d38eac8436dc5a73169f390a86f14354
habibullah <habibullahkhandokar01@gmail.com>
2025-09-30 18:01:10 +0600

    image banner slider update

--- src/app/components/ImageSlider/ImageSlider.jsx
+++ src/app/components/ImageSlider/ImageSlider.jsx
@@ -3,6 +3,9 @@
 import { useState } from "react";
 import ImageCard from "./ImageCard";
 
+import { FiChevronLeft } from "react-icons/fi";
+import { FiChevronRight } from "react-icons/fi";
+
 export default function ImageSlider({images}) {
 const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -22,13 +25,18 @@ const [currentIndex, setCurrentIndex] = useState(0);
   return (
     <>
     <div className="py-4">
-    <div className="rounded-lg h-72 mb-4 relative">
+    <div className="rounded-lg h-[27rem] mb-4 relative group">
       <img className="object-cover object-top rounded-lg w-full h-full" src={images[currentIndex]}/>
 
       <div className="absolute w-full top-1/2">
-      <div className="flex justify-between text-black">
-        <button onClick={()=>handleBacwordClick()}>after</button>
-        <button onClick={()=>handleForwordClick()}>before</button>
+      <div className="hidden justify-between duration-1000 text-xl p-2 group-hover:flex">
+        <button
+        className="p-2 rounded-full bg-black/55 "
+         onClick={()=>handleBacwordClick()}><FiChevronLeft /></button>
+
+        <button
+        className="p-2 rounded-full bg-black/55 "
+         onClick={()=>handleForwordClick()}><FiChevronRight /></button>
       </div>
       </div>
     </div>
@@ -39,8 +47,7 @@ const [currentIndex, setCurrentIndex] = useState(0);
         <div
         className={`
         hover:border-3 hover:border-red-500 rounded-lg border-2 border-transparent duration-300
-        ${idx === currentIndex ? 'border-3 border-red-500 rounded-lg':' border-2 border-transparent'}
-        `}
+        ${idx === currentIndex ? 'border-3 border-red-500 rounded-lg':' border-2 border-transparent'}`}
         onClick={() => handleImageClick(idx)}
         >
         <ImageCard img={image} idx={idx}/>
